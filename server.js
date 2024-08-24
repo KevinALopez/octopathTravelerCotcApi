f4128bdfb0b97bd6880463de4e3edfc79f4f114f
KevinALopez <005.kevin.ayala@gmail.com>
2024-08-24 15:37:52 +0200

    Update cookie maxAge

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
         secret: process.env.SESSIONSECRET,
         resave: false,
         saveUninitialized: false,
-        cookie: { maxAge: 1000 * 60 * 60 * 12 },
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
     })
 );
 
